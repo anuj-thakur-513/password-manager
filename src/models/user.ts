b69d5fc6ae1864ff8547c25be8204be73d42fb71
anuj-thakur-513 <anujthakur2003.anuj@gmail.com>
2024-07-07 17:22:58 +0530

    add User Model

---
+++ src/models/user.ts
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    googleId: {
+      type: String,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next): Promise<void> {
+  if (!this.isModified(this.password)) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+export const User = mongoose.model("user", userSchema);
