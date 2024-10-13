204e82001749c6f09f0234dcd58fc39a815d18c0
anuj-thakur-513 <anujthakur2003.anuj@gmail.com>
2024-10-13 14:01:37 +0530

    feat: update rate limiter

--- src/middlewares/rateLimiter.ts
+++ src/middlewares/rateLimiter.ts
@@ -1,7 +1,7 @@
 import { rateLimit } from "express-rate-limit";
 
 const rateLimiter = rateLimit({
-    windowMs: 30 * 60 * 1000, // 30 minutes
+    windowMs: 10 * 60 * 1000, // 10 minutes
     limit: 3, // Limit each IP to 3 requests per `window` (here, per 30 minutes).
     standardHeaders: "draft-7", // draft-7: combined `RateLimit` header
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
